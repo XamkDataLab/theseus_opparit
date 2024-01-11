f0069d8151edfb3b0fb70cbc12d310be21908059
Xamk DataLab <123467724+XamkDataLab@users.noreply.github.com>
2024-01-11 13:51:35 +0200

    Update queries.py

--- queries.py
+++ queries.py
@@ -8,7 +8,7 @@ database = st.secrets["database"]
 username = st.secrets["username"]
 password = st.secrets["password"]
 
-def get_company_names2():
+def get_theseus_data():
     query = "SELECT * FROM theseusAMK;"
     with pyodbc.connect(f'DRIVER={driver};SERVER={server};PORT=1433;DATABASE={database};UID={username};PWD={password}') as conn:
         df = pd.read_sql(query, conn)
