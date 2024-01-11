e4c2015f17a51a25989013b94b3b46680eed32ff
Xamk DataLab <123467724+XamkDataLab@users.noreply.github.com>
2024-01-11 13:45:56 +0200

    Create queries.py

---
+++ queries.py
@@ -0,0 +1,15 @@
+import pyodbc
+import streamlit as st
+import pandas as pd
+
+driver = st.secrets["driver"]
+server = st.secrets["server"]
+database = st.secrets["database"]
+username = st.secrets["username"]
+password = st.secrets["password"]
+
+def get_company_names2():
+    query = "SELECT * FROM theseusAMK;"
+    with pyodbc.connect(f'DRIVER={driver};SERVER={server};PORT=1433;DATABASE={database};UID={username};PWD={password}') as conn:
+        df = pd.read_sql(query, conn)
+    return df
