c0883d0715947119920b2b94daad91ce0859fe7d
Xamk DataLab <123467724+XamkDataLab@users.noreply.github.com>
2024-01-11 14:37:38 +0200

    Update etusivu.py

--- etusivu.py
+++ etusivu.py
@@ -29,7 +29,33 @@ page = st.sidebar.selectbox(
         '4'
         ]
 )
+def plot_values():
+    plot = st.selectbox(
+        'Valitse kuvaaja',
+        ['Opinnäytetöiden lukumäärän muutos', 
+         'Opinnäytetöiden määrät oppilaitoksittain',
+         'Opinnäytetöiden määrät oppilaitoksittain 2',
+         'Opinnäytetöiden lukumäärän vertailu vuosittain'
+         ]
+    )
+     
+    if plot == 'Opinnäytetöiden lukumäärän muutos':
+        monthly_filtered_data = filter_monthly(df)
+        plot_line(monthly_filtered_data)
+        
+    if plot == 'Opinnäytetöiden määrät oppilaitoksittain': 
+        yearly_sorted_data=filter_year_institution(df)
+        selected_institutions_data=select_institution(yearly_sorted_data)
+        plot_line_institutions(selected_institutions_data)
 
+    if plot == "Opinnäytetöiden määrät oppilaitoksittain 2":
+        yearly_sorted_data= filter_year_institution_sorted(df)
+        plot_scatter(yearly_sorted_data)
+
+    if plot == 'Opinnäytetöiden lukumäärän vertailu vuosittain':
+        yearly_sorted_data=filter_yearly(df)
+        df_yearly_filtered = year_slider(yearly_sorted_data)
+        plot_bar(df_yearly_filtered)
 if page == 'Theseus aineisto':
     st.header('Theseus aineiston analysointi')
     st.markdown('Taulukko opinnäytetöistä')
