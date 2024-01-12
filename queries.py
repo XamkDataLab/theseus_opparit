adf591efd8ed797f5dfc99001d18bd2b25d8134f
Xamk DataLab <123467724+XamkDataLab@users.noreply.github.com>
2024-01-12 12:33:05 +0200

    Update queries.py

--- queries.py
+++ queries.py
@@ -9,7 +9,7 @@ username = st.secrets["username"]
 password = st.secrets["password"]
 
 def get_theseus_data():
-    query = "SELECT julkaisupäivä, koulutusohjelma, koulutusala_fi, oppilaitos FROM theseusAMK;"
+    query = "SELECT id, julkaisupäivä, koulutusohjelma, koulutusala_fi, oppilaitos FROM theseusAMK;"
     with pyodbc.connect(f'DRIVER={driver};SERVER={server};PORT=1433;DATABASE={database};UID={username};PWD={password}') as conn:
         df = pd.read_sql(query, conn)
     return df
